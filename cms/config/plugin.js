dfe65fa69a725c9eb89d5807ab6de7b8aa0b7db4
Justin Donggyu Lee <34882520+nninnnin@users.noreply.github.com>
2023-06-15 22:40:23 +0900

    Update plugin settings

--- cms/config/plugin.js
+++ cms/config/plugin.js
@@ -15,4 +15,12 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      apolloServer: {
+        introspection: true,
+      },
+    },
+  },
 });
