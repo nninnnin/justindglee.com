50d884f91920ab7ed31d20390a30c0e598c9c9b5
nninnnin <nninnnin7@gmail.com>
2023-07-27 01:52:43 +0900

    Fix responsive styles

--- blog/src/components/Layout.tsx
+++ blog/src/components/Layout.tsx
@@ -9,11 +9,11 @@ interface Props {
 function Layout({ children, hasPadding = true }: Props) {
   return (
     <div className={`bg-blue-600 flex w-screen h-screen`}>
-      <div className="w-[200px] h-full fixed">
+      <div className="nav-wrapper w-[200px] h-full fixed">
         <Navigation />
       </div>
 
-      <div className="flex-1 ml-[200px] py-10">
+      <div className="contents-viewer-wrapper flex-1 ml-[200px] py-10">
         <div
           className={`contents-viewer glassmorph overflow-auto mx-auto w-[640px] h-full ${
             hasPadding && "p-[30px] pt-[26px]"
