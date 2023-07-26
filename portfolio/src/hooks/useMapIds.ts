cd38ec8f6876e200ea5ebde29d2c6a4472a682e7
이동규(Justin) <34882520+nninnnin@users.noreply.github.com>
2023-07-26 22:52:20 +0900

    Fix 'useMapId' hook to use memoization

--- portfolio/src/hooks/useMapIds.ts
+++ portfolio/src/hooks/useMapIds.ts
@@ -1,13 +1,17 @@
 import { map } from "fxjs";
+import { useMemo } from "react";
 import { v4 as uuid } from "uuid";
 
 export default function useMapIds(list: {}[]) {
-  const result = map((elem) => {
-    return {
-      id: uuid(),
-      ...elem,
-    };
-  }, list);
+  const result = useMemo(
+    map((elem) => {
+      return {
+        id: uuid(),
+        ...elem,
+      };
+    }, list),
+    [list]
+  );
 
   return result;
 }
