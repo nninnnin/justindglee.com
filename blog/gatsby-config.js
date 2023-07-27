dbf1ad27ad676de40b378224c3b1b86a2673e85e
이동규(Justin) <34882520+nninnnin@users.noreply.github.com>
2023-07-27 19:25:44 +0900

    Update ga settings: sending page view

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -54,7 +54,12 @@ module.exports = {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: ["5935086443", "G-6HB50XGJKT"],
+        trackingIds: ["G-6HB50XGJKT"],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          send_page_view: true,
+        },
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
