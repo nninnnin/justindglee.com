0269c37709e29f0a8ee209e72da6f11c5520688c
nninnnin <nninnnin7@gmail.com>
2023-09-05 04:29:25 +0900

    📮 Add endpoint: Get a post by slug

--- cms/src/api/post/controllers/post.js
+++ cms/src/api/post/controllers/post.js
@@ -1,9 +1,26 @@
-'use strict';
+"use strict";
 
 /**
  *  post controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::post.post');
+module.exports = createCoreController("api::post.post", ({ strapi }) => {
+  return {
+    async findBySlug(ctx) {
+      const { slug } = ctx.params;
+
+      const query = {
+        filters: { slug },
+        ...ctx.query,
+      };
+
+      const post = await strapi.entityService.findMany("api::post.post", query);
+
+      const sanitizedEntity = await this.sanitizeOutput(post);
+
+      return this.transformResponse(sanitizedEntity[0]);
+    },
+  };
+});
