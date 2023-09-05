8fc4eb1f128476f6e25c4f60ae2e6f98efc56903
이동규(Justin) <34882520+nninnnin@users.noreply.github.com>
2023-09-05 21:25:59 +0900

    👋 Remove Strapi (Separated as another repository)

--- cms/src/api/post/routes/routes.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  routes: [
-    {
-      method: "GET",
-      path: "/post/find-by-slug/:slug",
-      handler: "api::post.post.findBySlug",
-    },
-  ],
-};
--- cms/src/api/reference/routes/reference.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * reference router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::reference.reference');
