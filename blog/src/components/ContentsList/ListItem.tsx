ec5c69b01a6078a261d503fc5f031710453be26f
이동규(Justin) <34882520+nninnnin@users.noreply.github.com>
2024-01-20 23:43:19 +0900

    validDOMNesting 에러 해결을 위해 li -> div로 변경

--- blog/src/components/ContentsList/ListItem.tsx
+++ blog/src/components/ContentsList/ListItem.tsx
@@ -56,7 +56,7 @@ const ListItem = ({ postId, children }: Props) => {
   };
 
   return (
-    <li
+    <div
       className={clsx(
         "glassmorph-listitem p-5 mb-3 rounded-lg",
         "flex justify-between",
@@ -65,7 +65,7 @@ const ListItem = ({ postId, children }: Props) => {
       onContextMenu={handleContext}
     >
       {children}
-    </li>
+    </div>
   );
 };
 
